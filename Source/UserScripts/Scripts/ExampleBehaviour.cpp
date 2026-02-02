8f6709551d6cbb048c7eedf60d7bdbc0b7993eba
minkee009 <minkee5004@gmail.com>
2026-02-02 16:02:57 +0900

    에셋 업데이트

--- Source/UserScripts/Scripts/ExampleBehaviour.cpp
+++ Source/UserScripts/Scripts/ExampleBehaviour.cpp
@@ -3,6 +3,7 @@
 #include "ExampleBehaviour.h"
 #include "rttr/registration"
 #include "rttr/detail/policies/ctor_policies.h"
+#include "MMMInput.h"
 
 RTTR_PLUGIN_REGISTRATION
 {
@@ -10,11 +11,11 @@ RTTR_PLUGIN_REGISTRATION
 	using namespace MMMEngine;
 
 	registration::class_<ExampleBehaviour>("ExampleBehaviour")
-        (rttr::metadata("wrapper_type_name", "ObjPtr<ExampleBehaviour>"))
-        .property("IsCustomBool",&ExampleBehaviour::isCustomBool)
-        .property("CustomFloat",&ExampleBehaviour::customFloat)
-        .property("CustomInt",&ExampleBehaviour::customInt)
-        .property("CustomString",&ExampleBehaviour::customString);
+		(rttr::metadata("wrapper_type_name", "ObjPtr<ExampleBehaviour>"))
+		.property("IsCustomBool",&ExampleBehaviour::basespeed)
+		.property("IsScoop", &ExampleBehaviour::is_Scoop)
+		;
+
 
 	registration::class_<ObjPtr<ExampleBehaviour>>("ObjPtr<ExampleBehaviour>")
 		.constructor(
@@ -26,8 +27,91 @@ RTTR_PLUGIN_REGISTRATION
 
 void MMMEngine::ExampleBehaviour::Start()
 {
+
 }
 
 void MMMEngine::ExampleBehaviour::Update()
 {
+	float x = 0.f, z = 0.f;
+	if (Input::GetKey(KeyCode::W)) z += 1.f;
+	if (Input::GetKey(KeyCode::S)) z -= 1.f;
+	if (Input::GetKey(KeyCode::D)) x += 1.f;
+	if (Input::GetKey(KeyCode::A)) x -= 1.f;
+
+	m_InputDir = Vector3(x, 0.f, z);
+}
+
+void MMMEngine::ExampleBehaviour::FixedUpdate()
+{
+	auto rb = GetComponent<RigidBodyComponent>();
+
+
+    Vector3 desiredVel = ComputeDesiredVelocity();
+
+    rb->SetLinearVelocity(desiredVel);
+
+    ApplyYawFromVelocity(desiredVel);
+}
+
+
+void MMMEngine::ExampleBehaviour::OnCollisionEnter(MMMEngine::CollisionInfo info)
+{
+	std::cout << "Enter" << std::endl;
+	/*m_HasWall = false;*/
+}
+
+Vector3 MMMEngine::ExampleBehaviour::ComputeDesiredVelocity()
+{
+	Vector3 dir = m_InputDir;
+	if (dir.LengthSquared() > 0.f)
+	{
+		dir.Normalize();
+		return dir * basespeed;
+	}
+	return Vector3(0, 0, 0);
+}
+
+
+void MMMEngine::ExampleBehaviour::ApplyYawFromVelocity(const Vector3& v)
+{
+	Vector3 f = v;
+	f.y = 0.f;
+
+	if (f.LengthSquared() < 1e-6f) return;
+	f.Normalize();
+
+	auto rb = GetComponent<RigidBodyComponent>();
+
+	float targetYaw = std::atan2(f.x, f.z);
+	// 좌표계에 맞춰 atan2 부호/축만 조정
+	if (!is_Scoop)
+	{
+		
+		Quaternion q = Quaternion::CreateFromAxisAngle(Vector3::Up, targetYaw);
+
+		
+		rb->SnapRotation(q); // 혹은 부드럽게 보간해서 SnapRotation
+	}
+	else
+	{
+		dt = TimeManager::Get().GetDeltaTime();
+		curYaw = rb->Px_GetYaw();
+		//forward = rb->Px_GetForward();
+
+		float delta = WrapPi(targetYaw - curYaw);
+		float maxStep = turnSpeed * dt;
+		delta = std::clamp(delta, -maxStep, maxStep);
+
+		rb->SnapRotation(
+			Quaternion::CreateFromAxisAngle(Vector3::Up, curYaw + delta)
+		);
+	}
+}
+
+float MMMEngine::ExampleBehaviour::WrapPi(float a)
+{
+	const float pi = 3.1415926535f;
+	while (a > pi) a -= 2.f * pi;
+	while (a < -pi) a += 2.f * pi;
+	return a;
 }
--- Source/UserScripts/Scripts/ExampleBehaviour.h
+++ Source/UserScripts/Scripts/ExampleBehaviour.h
@@ -2,6 +2,11 @@
 #include "ScriptBehaviour.h"
 #include "UserScriptsCommon.h"
 #include <string>
+#include "PhysxManager.h"
+#include <SimpleMath.h>
+#include <MMMTime.h>
+
+using namespace DirectX::SimpleMath;
 
 namespace MMMEngine
 {
@@ -13,18 +18,47 @@ namespace MMMEngine
     public:
         ExampleBehaviour()
         {
-            REGISTER_BEHAVIOUR_MESSAGE(Start)
-            REGISTER_BEHAVIOUR_MESSAGE(Update)
+        REGISTER_BEHAVIOUR_MESSAGE(FixedUpdate);
+        REGISTER_BEHAVIOUR_MESSAGE(Start);
+        REGISTER_BEHAVIOUR_MESSAGE(Update);
+
         }
+        
+        float basespeed = 1.0f;
+        bool isSlow = false;
+        float yawRadians = 0.0f;
 
-        bool isCustomBool = true;
-        float customFloat = 14.0f;
-        int customInt = 2.0f;
-        std::string customString = "Hello, World!";
+        float maxYawSpeed = 0.0f;
 
-        bool isDontShowBool = false;
+        //PID제어
+        float kp = 0.0f;
+
+        //라디안
+        float alignEps;
 
         void Start();
         void Update();
+
+        //수치조절
+        float turnSpeed = 25.0f;
+        bool is_Scoop = false;
+
+
+        Vector3 m_InputDir;            // Update에서 저장 (정규화 전/후 상관없음)
+
+        void FixedUpdate();
+
+        void OnCollisionEnter(MMMEngine::CollisionInfo info);
+
+
+        Vector3 ComputeDesiredVelocity();
+        void ApplyYawFromVelocity(const Vector3& v);
+
+        float WrapPi(float a);
+
+        float curYaw = 0.0f;
+        //Vector3 forward{};
+        float dt = 0.0f;
+
     };
 }
