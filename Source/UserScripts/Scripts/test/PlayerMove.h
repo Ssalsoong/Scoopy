f591658bcc400ca80e1c4c1d70fa5bf9b8b49997
Foodie_Pudding <sacar1920@gmail.com>
2026-02-08 17:37:42 +0900

    [FIX] PlayerMove Update

--- Source/UserScripts/Scripts/test/PlayerMove.h
+++ Source/UserScripts/Scripts/test/PlayerMove.h
@@ -32,7 +32,7 @@ namespace MMMEngine
         float basespeed = 80.0f;
 
         USCRIPT_PROPERTY()
-        bool isSlow = false;
+            bool isSlow = false;
         float yawRadians = 0.0f;
 
         float maxYawSpeed = 0.0f;
@@ -44,22 +44,22 @@ namespace MMMEngine
         float alignEps;
 
         USCRIPT_MESSAGE()
-        void Start();
+            void Start();
 
 
         //수치조절
         USCRIPT_PROPERTY()
-        float turnSpeed = 25.0f;
+            float turnSpeed = 25.0f;
 
         USCRIPT_PROPERTY()
-        bool is_Scoop = false;
+            bool is_Scoop = false;
         ObjPtr<GameObject> Snow;
 
-        
+
         bool m_LookTarget = false;
 
         USCRIPT_MESSAGE()
-        void FixedUpdate();
+            void FixedUpdate();
 
 
         DirectX::SimpleMath::Vector3 ComputeDesiredVelocity();
@@ -94,15 +94,19 @@ namespace MMMEngine
         float ComputeSpeed();
 
         USCRIPT_PROPERTY()
-        float DefaultSpeed = 80.f;
+            float DefaultSpeed = 80.f;
         USCRIPT_PROPERTY()
-        float OnSnowSpeed = 60.f;
+            float OnSnowSpeed = 60.f;
 
         USCRIPT_PROPERTY()
-        float MinusSpeed = 1.5f;
+            float MinusSpeed = 1.5f;
 
         USCRIPT_PROPERTY()
-        float MinSpeed = 35.f;
+            float MinSpeed = 35.f;
+
+        float CurSpeed = 0;
+
+        float GetCurSpeed();
 
     };
 }
