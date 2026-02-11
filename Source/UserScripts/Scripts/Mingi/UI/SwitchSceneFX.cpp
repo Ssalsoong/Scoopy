d2164415492ea4ac99462aa22b222a49eaa6865b
minkee009 <minkee5004@gmail.com>
2026-02-11 12:27:41 +0900

    SceneSwitcher 작성중

---
+++ Source/UserScripts/Scripts/Mingi/UI/SwitchSceneFX.cpp
@@ -0,0 +1,64 @@
+﻿#include "Export.h"
+#include "ScriptBehaviour.h"
+#include "SwitchSceneFX.h"
+#include "MMMTime.h"
+#include "Image.h"
+#include "Canvas.h"
+
+using namespace MMMEngine;
+
+ObjPtr<SwitchSceneFX> SwitchSceneFX::Instance = nullptr;
+
+
+void MMMEngine::SwitchSceneFX::Awake()
+{
+	if (!Instance.IsValid())
+	{
+		Instance = SelfPtr(this);
+		DontDestroyOnLoad(SelfPtr(this));
+	}
+	else
+	{
+		GetGameObject()->Destroy(GetGameObject());
+	}
+}
+
+void MMMEngine::SwitchSceneFX::OnDestroy()
+{
+	if (Instance == SelfPtr(this))
+		Instance = nullptr;
+}
+
+void MMMEngine::SwitchSceneFX::Update()
+{
+	// idle 시 리턴
+	if (m_animState == 0)
+		return;
+
+	m_timer += Time::GetUnscaledDeltaTime();
+
+	switch (m_animState)
+	{
+		// 가리기 시작
+	case 1:
+		
+		break;
+
+		// 화면 보이게
+	case 2:
+		break;
+	}
+}
+
+int MMMEngine::SwitchSceneFX::GetState()
+{
+	return m_animState;
+}
+
+void MMMEngine::SwitchSceneFX::FXStart()
+{
+}
+
+void MMMEngine::SwitchSceneFX::FXEnd()
+{
+}
---
+++ Source/UserScripts/Scripts/Mingi/UI/SwitchSceneFX.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "rttr/type"
+#include "ScriptBehaviour.h"
+#include "UserScriptsCommon.h"
+#include "AnimationCurve.h"
+#include "SimpleMath.h"
+
+namespace MMMEngine
+{
+    class Image;
+    class Canvas;
+    class USERSCRIPTS SwitchSceneFX : public ScriptBehaviour
+    {
+    private:
+        float m_timer = 0.0f;
+        int m_animState = 0; // 0 : idle, 1 : start, 2 : end;
+        DirectX::SimpleMath::Vector2 m_canvasSize;
+        RTTR_ENABLE(ScriptBehaviour)
+        RTTR_REGISTRATION_FRIEND
+    public:
+        SwitchSceneFX()
+        {
+        }
+
+        USCRIPT_MESSAGE()
+        void Awake();
+
+        static ObjPtr<SwitchSceneFX> Instance;
+
+        USCRIPT_PROPERTY()
+        ObjPtr<Image> FXImage;
+
+        USCRIPT_PROPERTY()
+        AnimationCurve FXCurve;
+
+        USCRIPT_PROPERTY()
+            ObjPtr<Canvas> ParentCanvas;
+
+        USCRIPT_MESSAGE()
+        void OnDestroy();
+
+        USCRIPT_MESSAGE()
+        void Update();
+
+        int GetState();
+
+
+        USCRIPT_MESSAGE()
+            void FXStart();
+
+        USCRIPT_MESSAGE()
+            void FXEnd();
+    };
+}
