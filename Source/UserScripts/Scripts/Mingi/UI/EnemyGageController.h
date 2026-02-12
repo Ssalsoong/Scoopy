a8e8f5e019a09b96ed5d73e2dd53118978225ea4
minkee009 <minkee5004@gmail.com>
2026-02-13 03:35:10 +0900

    에너미 HP 출력 가능, 일부 WorldSpaceUI 1프레임 동기화 비일치 버그 해결

---
+++ Source/UserScripts/Scripts/Mingi/UI/EnemyGageController.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "rttr/type"
+#include "ScriptBehaviour.h"
+#include "UserScriptsCommon.h"
+
+namespace MMMEngine
+{
+    class Battlestats;
+    class Gage;
+    class USERSCRIPTS EnemyGageController : public ScriptBehaviour
+    {
+    private:
+        RTTR_ENABLE(ScriptBehaviour)
+        RTTR_REGISTRATION_FRIEND
+    public:
+        EnemyGageController()
+        {
+        REGISTER_BEHAVIOUR_MESSAGE(OnDisable);
+        REGISTER_BEHAVIOUR_MESSAGE(OnEnable);
+        REGISTER_BEHAVIOUR_MESSAGE(Update);
+
+        }
+
+        USCRIPT_PROPERTY()
+            ObjPtr<Battlestats> battlestats;
+
+        ObjPtr<Gage> hpUI;
+
+        enum class EnemyType { Warrior, Ranger, Assassin };
+
+        EnemyType type;
+
+        USCRIPT_MESSAGE()
+        void OnEnable();
+
+        USCRIPT_MESSAGE()
+        void OnDisable();
+
+        USCRIPT_MESSAGE()
+        void Update();
+    };
+}
--- Source/UserScripts/Scripts/Mingi/UI/MarkWaveController.cpp
+++ Source/UserScripts/Scripts/Mingi/UI/MarkWaveController.cpp
@@ -21,7 +21,6 @@ void MMMEngine::MarkWaveController::Awake()
 	if (!Instance.IsValid())
 	{
 		Instance = SelfPtr(this);
-		DontDestroyOnLoad(SelfPtr(this));
 	}
 	else
 	{
@@ -40,114 +39,139 @@ void MMMEngine::MarkWaveController::SetWave(int wave)
 	case 1:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num2_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("5");
 		break;
 	case 2:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num4_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("10");
 		break;
 	case 3:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num6_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("10");
 
 		Ranger1->GetGameObject()->SetActive(true);
 		Ranger1->worldSpaceUI->TargetTransform = num2_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger1->countText->SetTextUtf8("10");
 		break;
 	case 4:
 		Assassin1->GetGameObject()->SetActive(true);
 		Assassin1->worldSpaceUI->TargetTransform = num4_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Assassin1->countText->SetTextUtf8("10");
 		break;
 	case 5:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num8_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("10");
 
 		Ranger1->GetGameObject()->SetActive(true);
 		Ranger1->worldSpaceUI->TargetTransform = num9_UP_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger1->countText->SetTextUtf8("10");
 		break;
 	case 6:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num6_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("15");
 
 		Assassin1->GetGameObject()->SetActive(true);
 		Assassin1->worldSpaceUI->TargetTransform = num4_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Assassin1->countText->SetTextUtf8("10");
 		break;
 
 	case 7:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num2_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("15");
 
 		Ranger1->GetGameObject()->SetActive(true);
 		Ranger1->worldSpaceUI->TargetTransform = num1_L_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger1->countText->SetTextUtf8("10");
 
 		Ranger2->GetGameObject()->SetActive(true);
 		Ranger2->worldSpaceUI->TargetTransform = num3_R_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger2->countText->SetTextUtf8("10");
 		break;
 	case 8:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num6_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("10");
 
 		Ranger1->GetGameObject()->SetActive(true);
 		Ranger1->worldSpaceUI->TargetTransform = num3_R_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger1->countText->SetTextUtf8("10");
 
 		Ranger2->GetGameObject()->SetActive(true);
 		Ranger2->worldSpaceUI->TargetTransform = num1_L_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger2->countText->SetTextUtf8("10");
 
 		Assassin1->GetGameObject()->SetActive(true);
 		Assassin1->worldSpaceUI->TargetTransform = num4_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Assassin1->countText->SetTextUtf8("10");
 		break;
 
 	case 9:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num8_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("15");
 
 		Warrior2->GetGameObject()->SetActive(true);
 		Warrior2->worldSpaceUI->TargetTransform = num6_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior2->countText->SetTextUtf8("15");
 
 		Ranger1->GetGameObject()->SetActive(true);
 		Ranger1->worldSpaceUI->TargetTransform = num9_R_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger1->countText->SetTextUtf8("10");
 
 		Assassin1->GetGameObject()->SetActive(true);
 		Assassin1->worldSpaceUI->TargetTransform = num2_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Assassin1->countText->SetTextUtf8("10");
 		break;
 
 	case 10:
 		Warrior1->GetGameObject()->SetActive(true);
 		Warrior1->worldSpaceUI->TargetTransform = num8_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior1->countText->SetTextUtf8("20");
 
 		Warrior2->GetGameObject()->SetActive(true);
 		Warrior2->worldSpaceUI->TargetTransform = num4_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Warrior2->countText->SetTextUtf8("20");
 
 		Ranger1->GetGameObject()->SetActive(true);
 		Ranger1->worldSpaceUI->TargetTransform = num7_UP_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger1->countText->SetTextUtf8("10");
 
 		Ranger2->GetGameObject()->SetActive(true);
 		Ranger2->worldSpaceUI->TargetTransform = num6_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Ranger2->countText->SetTextUtf8("10");
 
 		Assassin1->GetGameObject()->SetActive(true);
 		Assassin1->worldSpaceUI->TargetTransform = num2_TR;
+		Warrior1->worldSpaceUI->LateUpdate();
 		Assassin1->countText->SetTextUtf8("10");
 		break;
 	}
